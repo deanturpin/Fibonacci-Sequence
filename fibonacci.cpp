16fe80938b993d934792dbdac61f5a2bf596d0e8
deanturpin <git@lvbtn.uk>
2016-09-22 17:38:47 +0100

    fix the sequence

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
-int fibonacci(const unsigned int);
+unsigned int fibonacci(const unsigned int);
 
 int main() {
 
-	return fibonacci(5);
+	std::cout << fibonacci(5) << std::endl;
+
+	return 0;
 }
 
-int fibonacci(const unsigned int n) {
+unsigned int fibonacci(const unsigned int n) {
 
 	if (n == 1)
 		return 1;
 
-	return fibonacci(n - 1);
+	return n * fibonacci(n - 1);
 }
