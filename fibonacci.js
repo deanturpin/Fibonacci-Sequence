9730cd75d73a8d7f1ded4b0b89bee2689c56c7ac
deanturpin <git@lvbtn.uk>
2016-09-27 19:16:01 +0100

    quick version with expanded input sequence

--- fibonacci.js
+++ fibonacci.js
@@ -7,4 +7,20 @@ function fibonacci(n) {
 	)
 }
 
-console.log(fibonacci(10))
+console.log(
+	fibonacci(0),
+	fibonacci(1),
+	fibonacci(2),
+	fibonacci(3),
+	fibonacci(4),
+	fibonacci(5),
+	fibonacci(6),
+	fibonacci(7),
+	fibonacci(8),
+	fibonacci(9),
+	fibonacci(10),
+	fibonacci(11),
+	fibonacci(12),
+	fibonacci(13),
+	fibonacci(14)
+)
