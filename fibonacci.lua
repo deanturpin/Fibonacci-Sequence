30a86109fe7557ff1460212d53873d50d39dc642
deanturpin <git@lvbtn.uk>
2016-10-13 17:38:16 +0100

    remove else

--- fibonacci.lua
+++ fibonacci.lua
@@ -2,10 +2,9 @@
 
 function fibonacci(n)
 
-	if n < 2 then return n
-	else return fibonacci(n - 1) + fibonacci(n - 2)
-	end
+	if n < 2 then return n end
 
+	return fibonacci(n - 1) + fibonacci(n - 2)
 end
 
 print("lua", fibonacci(14))
